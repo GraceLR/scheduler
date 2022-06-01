4756f30875eeb654788f47fee10acf86b75d4f94
GraceLR <gracerwork08@gmail.com>
2022-06-01 17:15:37 -0400

    edit Appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 import "./styles.scss";
 
-export default function Appointment(props) {
+export default function Appointment({time, interview}) {
     return(
-        <article className="appointment"></article> 
+        <article className="appointment">
+            <Header time={time}/>
+            {interview ? 
+            <Show student={interview.student} interviewer={interview.interviewer} /> : 
+            <Empty />}
+        </article> 
     )
 };
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment }  from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -154,7 +154,23 @@ storiesOf("Appointment", module)
   .add("Confirm onCancel", () => <Confirm student="GLR" message="Delete the appointment?" onCancel={action("onCancel")} />)
   .add("Confirm onDelete", () => <Confirm student="GLR" message="Delete the appointment?" onConfirm={action("onConfirm")} />)
   .add("Form Edit", () => <Form student="GLR" interviewer={interviewer} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
-  .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />);
+  .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ));
 
 storiesOf("Status", module)
   .addParameters({

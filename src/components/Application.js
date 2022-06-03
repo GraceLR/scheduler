766b20554f9ac1ae4a3e2274f160e5b31982a5e7
GraceLR <gracerwork08@gmail.com>
2022-06-03 13:01:38 -0400

    add interviewers to state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,7 +31,7 @@ export default function Application() {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
   return (

f8c6252ce1186351dcccfcfeb6e298880b26c217
GraceLR <gracerwork08@gmail.com>
2022-06-07 18:32:14 -0400

    Separation of Concerns for Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,50 +1,20 @@
 import React, { Fragment, useState, useEffect } from "react";
-import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+
+import useApplicationData from "../hooks/useApplicationData";
 
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
 
 import "components/Application.scss";
 
 export default function Application() {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
-  }, []);
-  const bookInterview = (id, interview, from) => {
-    const days = [...state.days].map(day => {
-      if (day.appointments.includes(id) && from === 'CREATE') {
-        day.spots--;
-      }
-      return day;
-    });
-    const appointment = {...state.appointments[id], interview: { ...interview }};
-    const appointments = {...state.appointments, [id]: appointment};
-    setState(prev => ({...prev, days: days, appointments: appointments}));
-  };
-  const cancelInterview = id => {
-    const daysNew = [...state.days].map(day => {
-      if (day.appointments.includes(id)) {
-        day.spots++;
-      }
-      return day;
-    });
-    let appointmentsnew = {...state.appointments};
-    appointmentsnew[id].interview = null;
-    setState(prev => ({...prev, days: daysNew, appointments: appointmentsnew}));
-  };
+  const {
+    state,
+    setState,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
   const schedule = appointments.map((appointment) => {

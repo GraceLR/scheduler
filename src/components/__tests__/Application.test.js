463198dc23144b20bb05520db06ebc58f43ad160
GraceLR <gracerwork08@gmail.com>
2022-06-15 19:23:33 -0400

    jest tests fixed

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -84,19 +84,34 @@ describe("Form", () => {
   
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
-    console.log(prettyDOM(appointment))
   
     fireEvent.click(getByAltText(appointment, "Add"));
-  
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-  
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
-    // expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "ERROR_SAVE"));
     expect(getByText(appointment, "ERROR_SAVE")).toBeInTheDocument();
   
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[1];
+  
+    fireEvent.click(getByAltText(appointment, "Delete"));
+    fireEvent.click(getByText(appointment, "Confirm"));
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "ERROR_DELETE"));
+    expect(getByText(appointment, "ERROR_DELETE")).toBeInTheDocument();
+
   });
 
 })
